ed594a5df11bb2578e8b5e61c24b2b5239738e65
nandhu0424 <nandhinin2004@gmail.com>
2025-10-18 19:25:14 +0530

    api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://contact-management-backend-suff.onrender.com/"
 });
 
 // Add token automatically if available
